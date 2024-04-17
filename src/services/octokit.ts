881929d1e61fbbcbc632c53981d1ed5fc7d934fc
Riku Rauhala <riku@rauhala.io>
2024-04-17 13:32:11 +0300

    Store data in session storage to prevent unnecessary API calls

--- src/services/octokit.ts
+++ src/services/octokit.ts
@@ -6,39 +6,62 @@ import { RepositoryFull, UserFull } from '~/types'
 const octokit = new Octokit({ auth: TOKEN })
 
 const getLanguages = (languagesUrl: string, setState: (arg0: string[]) => void) => {
+  const languages = sessionStorage.getItem(`languages${languagesUrl}`)
+  if (languages) {
+    setState(JSON.parse(languages) as string[])
+    return
+  }
+
   const fetchLanguages = async () => {
     try {
-      const octokit = new Octokit()
       const response = await octokit.request(`GET ${languagesUrl}`)
       setState(response.data as Array<string>)
+      sessionStorage.setItem(`languages${languagesUrl}`, JSON.stringify(response.data))
     } catch (error) {
       console.error(error)
     }
   }
+
   void fetchLanguages()
 }
 
 const getRepositories = (setState: (arg0: RepositoryFull[]) => void) => {
+  const repositories = sessionStorage.getItem('repositories')
+  if (repositories) {
+    setState(JSON.parse(repositories) as RepositoryFull[])
+    return
+  }
+
   const fetchRepositories = async () => {
     try {
-      const response = await octokit.request(`GET /user/repos`)
+      const response = await octokit.request('GET /user/repos')
       setState(response.data as Array<RepositoryFull>)
+      sessionStorage.setItem('repositories', JSON.stringify(response.data))
     } catch (error) {
       console.error(error)
     }
   }
+
   void fetchRepositories()
 }
 
 const getUser = (setState: (arg0: UserFull) => void) => {
+  const gitHubUser = sessionStorage.getItem('gitHubUser')
+  if (gitHubUser) {
+    setState(JSON.parse(gitHubUser) as UserFull)
+    return
+  }
+
   const fetchUser = async () => {
     try {
       const response = await octokit.request('GET /user')
       setState(response.data as UserFull)
+      sessionStorage.setItem('gitHubUser', JSON.stringify(response.data))
     } catch (error) {
       console.error(error)
     }
   }
+
   void fetchUser()
 }
 
