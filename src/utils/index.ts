86405676942d6e3fdc7df8db9c5cc404197fd5e8
Riku Rauhala <riku@rauhala.io>
2024-04-19 21:08:11 +0300

    Use only colors with enough contrast for non-language topics

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import ColorContrastChecker from 'color-contrast-checker'
+import theme from '~/theme'
 import colorMap from './colors.json'
 
 const colors: { [key: string]: string } = {}
@@ -8,8 +10,31 @@ for (const key in colorMap) {
   }
 }
 
+const colorContrastChecker = new ColorContrastChecker()
+
+/**
+ * Returns a random color from a predefined list.
+ *
+ * @returns {string} The color in hexadecimal format.
+ * @example getRandomColor() // Returns '#f1e05a'
+ */
 const getRandomColor = (): string => {
   return colors[Object.keys(colors)[Math.floor(Math.random() * Object.keys(colors).length)]]
+}
+
+/**
+ * Returns a random color that has a high contrast with the background.
+ *
+ * @returns {string} The color in hexadecimal format.
+ * @example getRandomContrastColor() // Returns '#f1e05a'
+ */
+const getRandomContrastColor = (): string => {
+  let contrastColor = getRandomColor()
+  const backgroundColor = theme.palette.background.paper
+  while (!colorContrastChecker.isLevelAA(backgroundColor, contrastColor)) {
+    contrastColor = getRandomColor()
+  }
+  return contrastColor
 }
 
 /**
@@ -29,11 +54,14 @@ export const formatTimestamp = (timestamp: string): string => {
 
 /**
  * Returns a predetermined color for a given key.
+ * If a key is not found, a random color is returned.
+ *
+ * The colors are based on the GitHub language colors.
  *
  * @param {string} key - The key to get the color for.
  * @returns {string} The color in hexadecimal format.
  * @example getColor('JavaScript') // Returns '#f1e05a'
  */
 export const getColor = (key: string): string => {
-  return colors[key.toLowerCase() as keyof typeof colors] || getRandomColor()
+  return colors[key.toLowerCase() as keyof typeof colors] || getRandomContrastColor()
 }
