ba2cb6ac6cf05d50238118dbf1f5b16941db3dc8
Riku Rauhala <riku@rauhala.io>
2024-04-19 19:23:38 +0300

    Add a slider for controlling the number of topics shown

--- src/components/Topics/Topics.tsx
+++ src/components/Topics/Topics.tsx
@@ -1,16 +1,14 @@
 import { useEffect, useState } from 'react'
 import ReactWordcloud from '@cyberblast/react-wordcloud'
 import Paper from '@mui/material/Paper'
+import Slider from '@mui/material/Slider'
 
 import dataService from '~/services/data'
 import { TopicMap } from '~/types'
 
 const Topics = (): JSX.Element => {
   const [topics, setTopics] = useState<TopicMap>({})
-
-  const words = Object.entries(topics)
-    .map(([topic, count]) => ({ text: topic, value: count }))
-    .slice(0, 50)
+  const [shown, setShown] = useState<number>(20)
 
   useEffect(() => {
     void dataService.getTopics().then((topicData) => {
@@ -19,20 +17,36 @@ const Topics = (): JSX.Element => {
   }, [])
 
   return (
-    <Paper elevation={3} sx={{ margin: '20px 0px', padding: '20px' }}>
-      <ReactWordcloud
-        callbacks={{
-          getWordTooltip: (word) => `${word.text} (${word.value})`,
-        }}
-        options={{
-          fontSizes: [20, 100],
-          rotations: 2,
-          rotationAngles: [-90, 0],
-          padding: 5,
-        }}
-        words={words}
+    <>
+      <Slider
+        marks={Array.from({ length: 5 }, (_, i) => ({
+          value: (i + 1) * 10,
+          label: ((i + 1) * 10).toString(),
+        }))}
+        max={50}
+        min={5}
+        onChange={(_event, value) => value && setShown(value as number)}
+        step={5}
+        value={shown}
+        valueLabelDisplay="auto"
       />
-    </Paper>
+      <Paper elevation={3} sx={{ margin: '20px 0px', padding: '20px' }}>
+        <ReactWordcloud
+          callbacks={{
+            getWordTooltip: (word) => `${word.text} (${word.value})`,
+          }}
+          options={{
+            fontSizes: [20, 100],
+            rotations: 2,
+            rotationAngles: [-90, 0],
+            padding: 5,
+          }}
+          words={Object.entries(topics)
+            .map(([topic, count]) => ({ text: topic, value: count }))
+            .slice(0, shown)}
+        />
+      </Paper>
+    </>
   )
 }
 
