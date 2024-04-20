c7778e995d153d3015a6b2dd118c882090329821
Riku Rauhala <riku@rauhala.io>
2024-04-20 21:19:32 +0300

    Update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,9 @@
+interface Author {
+  date: string
+  email: string
+  name: string
+}
+
 export interface GitHubUser {
   avatarUrl: string
   location: string
@@ -8,6 +14,88 @@ export interface GitHubUser {
 }
 
 export type ColorCode = `#${string}`
+
+interface Commit {
+  author: Author
+  comment_count: number
+  committer: Author | null
+  message: string
+  tree: Tree
+  url: string
+}
+
+export interface IssueOrPullRequest {
+  closedAt: string | null
+  createdAt: string
+  state: string
+  type: 'issue' | 'pullRequest'
+}
+
+export interface IssueOrPullRequestFromAPI {
+  active_lock_reason?: string | null | undefined
+  assignee: User | null
+  assignees?: User[] | null | undefined
+  author_association:
+    | 'COLLABORATOR'
+    | 'CONTRIBUTOR'
+    | 'FIRST_TIMER'
+    | 'FIRST_TIME_CONTRIBUTOR'
+    | 'MANNEQUIN'
+    | 'MEMBER'
+    | 'NONE'
+    | 'OWNER'
+  body?: string | undefined
+  closed_at: string | null
+  comments: number
+  comments_url: string
+  created_at: string
+  draft?: boolean | undefined
+  events_url: string
+  html_url: string
+  id: number
+  labels: Label[]
+  labels_url: string
+  locked: boolean
+  milestone: Milestone | null
+  node_id: string
+  number: number
+  performed_via_github_app: boolean | null
+  pull_request?: PullRequest | undefined
+  reactions?: Reactions | undefined
+  repository_url: string
+  score: number
+  state: string
+  state_reason?: string | null | undefined
+  text_matches?:
+    | {
+        object_url?: string | undefined
+        object_type?: string | null | undefined
+        property?: string | undefined
+        fragment?: string | undefined
+        matches?:
+          | {
+              text?: string | undefined
+              indices?: number[] | undefined
+            }[]
+          | undefined
+      }[]
+    | undefined
+  timeline_url: string
+  title: string
+  updated_at: string
+  url: string
+  user: User | null
+}
+
+interface Label {
+  color?: string | undefined
+  default?: boolean | undefined
+  description?: string | null | undefined
+  id?: number | undefined
+  name?: string | undefined
+  node_id?: string | undefined
+  url?: string | undefined
+}
 
 export interface LanguageMap {
   [language: string]: number
@@ -21,6 +109,46 @@ interface License {
   url: string
 }
 
+interface Milestone {
+  closed_at: string | null
+  closed_issues: number
+  created_at: string
+  creator: User | null
+  description: string | null
+  due_on: string | null
+  html_url: string
+  id: number
+  labels_url: string
+  number: number
+  node_id: string
+  open_issues: number
+  state: 'open' | 'closed'
+  title: string
+  updated_at: string
+  url: string
+}
+
+interface PullRequest {
+  diff_url: string | null
+  html_url: string | null
+  merged_at?: string | null | undefined
+  patch_url: string | null
+  url: string | null
+}
+
+interface Reactions {
+  '-1': number
+  '+1': number
+  confused: number
+  eyes: number
+  heart: number
+  hooray: number
+  laugh: number
+  rocket: number
+  total_count: number
+  url: string
+}
+
 export interface Repository {
   description: string
   homepage: string
@@ -28,9 +156,9 @@ export interface Repository {
   id: number
   languages_url: string
   name: string
-  pushed_at: Date
-  topics: Array<string>
-  year: string
+  pushed_at: string
+  topics: string[]
+  year?: string
 }
 
 export interface RepositoryFull {
@@ -104,7 +232,7 @@ export interface RepositoryFull {
   svn_url: string
   tags_url: string
   teams_url: string
-  topics: Array<string>
+  topics: string[]
   trees_url: string
   updated_at: string
   url: string
@@ -120,19 +248,22 @@ export interface TopicMap {
 
 interface User {
   avatar_url: string
+  email?: string | null | undefined
   events_url: string
   followers_url: string
   following_url: string
   gists_url: string
-  gravatar_id: string
+  gravatar_id: string | null
   html_url: string
   id: number
   login: string
+  name?: string | null | undefined
   node_id: string
   organizations_url: string
   received_events_url: string
   repos_url: string
   site_admin: boolean
+  starred_at?: string | undefined
   starred_url: string
   subscriptions_url: string
   type: string
@@ -171,5 +302,31 @@ export interface UserFull {
   twitter_username: string
   type: string
   updated_at: string
+  url: string
+}
+
+interface CommitFromAPI {
+  author: User
+  comments_url: string
+  commit: Commit
+  committer: User | null
+  html_url: string
+  node_id: string
+  parents: Tree[]
+  repository: Repository
+  score: number
+  sha: string
+  url: string
+}
+
+export interface CommitsFromAPI {
+  total_count: number
+  incomplete_results: boolean
+  items: CommitFromAPI[] | null
+}
+
+interface Tree {
+  html_url?: string
+  sha: string
   url: string
 }
