035e0936f9570ab1bd5b60bee7d21fd794eafb8a
Riku Rauhala <riku@rauhala.io>
2024-05-09 20:35:15 +0300

    Update colors.json and linguist.py

--- docs/scripts/linguist.py
+++ docs/scripts/linguist.py
@@ -1,3 +1,4 @@
+import json
 import requests
 import yaml
 
@@ -9,19 +10,23 @@ if response.status_code != 200:
     print(f'Failed to fetch YAML file: {response.status_code}')
     exit()
 
-yaml_data = response.text
-languages = yaml.safe_load(yaml_data)
+data = yaml.safe_load(response.text)
+
+languages = {}
+
+for language, properties in data.items():
+    try:
+        color = properties['color'].lower()
+        languages[language] = color
+    except KeyError:
+        pass
+
+
+sorted_languages = {
+    language: languages[language] for language in sorted(languages.keys(), key=lambda key: key.lower())
+}
+
 with open(JSON_FILE, 'w') as json_file:
-    json_file.write('{\n')
-    for key, value in languages.items():
-        try:
-            color = value['color'].lower()
-            json_file.write(f'  "{key}": "{color}",\n')
-        except KeyError:
-            pass
-    json_file.seek(json_file.tell() - 2, 0)
-    json_file.truncate()
-    json_file.write('\n}\n')
+    json.dump(sorted_languages, json_file, indent=2)
 
 print(f'Data written to {JSON_FILE}')
-
