83f14ce1e6c3b1d342c6c2b699389cd6bd59fbbd
Riku Rauhala <riku@rauhala.io>
2024-05-11 12:50:26 +0300

    Add an option for viewing commits by month or by year

--- src/components/Commits/Commits.tsx
+++ src/components/Commits/Commits.tsx
@@ -1,4 +1,9 @@
+import Box from '@mui/material/Box'
+import FormControl from '@mui/material/FormControl'
+import FormControlLabel from '@mui/material/FormControlLabel'
 import Paper from '@mui/material/Paper'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
 import { useEffect, useState } from 'react'
 
 import dataService from '~/services/data'
@@ -8,8 +13,35 @@ import CommitsChart from './CommitsChart'
 import Loading from './Loading'
 import NoData from './NoData'
 
+enum Mode {
+  MONTH = 'month',
+  YEAR = 'year',
+}
+
+const getCommitCountsByMonth = (commits: Commit[]): Record<string, number> => {
+  return commits.reduce((counts, commit) => {
+    const commitDate = new Date(commit.date)
+    const monthYear = `${commitDate.getFullYear()}-${String(commitDate.getMonth() + 1).padStart(
+      2,
+      '0'
+    )}`
+    counts[monthYear] = (counts[monthYear] || 0) + 1
+    return counts
+  }, {} as Record<string, number>)
+}
+
+const getCommitCountsByYear = (commits: Commit[]): Record<string, number> => {
+  return commits.reduce((counts, commit) => {
+    const commitDate = new Date(commit.date)
+    const year = commitDate.getFullYear().toString()
+    counts[year] = (counts[year] || 0) + 1
+    return counts
+  }, {} as Record<string, number>)
+}
+
 const Commits = (): JSX.Element => {
   const [commits, setCommits] = useState<Commit[]>([])
+  const [mode, setMode] = useState<Mode>(Mode.MONTH)
   const [loading, setLoading] = useState<boolean>(true)
   const [noData, setNoData] = useState<boolean>(true)
 
@@ -49,51 +81,79 @@ const Commits = (): JSX.Element => {
     setLoading(false)
   }, [])
 
-  const commitCountsByMonth = commits.reduce((counts, commit) => {
-    const commitDate = new Date(commit.date)
-    const monthYear = `${commitDate.getFullYear()}-${String(commitDate.getMonth() + 1).padStart(
-      2,
-      '0'
-    )}`
-    counts[monthYear] = (counts[monthYear] || 0) + 1
-    return counts
-  }, {} as Record<string, number>)
-
-  const earliestDate = new Date(
-    Math.min(...Object.keys(commitCountsByMonth).map((key) => new Date(key).getTime()))
-  )
-  const latestDate = new Date(
-    Math.max(...Object.keys(commitCountsByMonth).map((key) => new Date(key).getTime()))
-  )
-
-  const monthsWithZeroCommits: Record<string, number> = {}
-  const currentDate = new Date(earliestDate)
-  while (currentDate <= latestDate) {
-    const monthYear = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(
-      2,
-      '0'
-    )}`
-    if (!(monthYear in commitCountsByMonth)) {
-      monthsWithZeroCommits[monthYear] = 0
+  const formatDate = (date: Date) => {
+    if (mode === Mode.MONTH) {
+      return date.toLocaleDateString('en-US', { month: '2-digit', year: 'numeric' })
     }
-    currentDate.setMonth(currentDate.getMonth() + 1)
+    return date.toLocaleDateString('en-US', { year: 'numeric' })
   }
 
-  const mergedCommitCounts = { ...monthsWithZeroCommits, ...commitCountsByMonth }
+  const getLabel = () => {
+    return mode === Mode.MONTH ? ' \nMonth' : ' \nYear'
+  }
+
+  const commitCountsByPeriod =
+    mode === Mode.MONTH ? getCommitCountsByMonth(commits) : getCommitCountsByYear(commits)
+
+  const earliestDate = new Date(
+    Math.min(...Object.keys(commitCountsByPeriod).map((key) => new Date(key).getTime()))
+  )
+  const latestDate = new Date(
+    Math.max(...Object.keys(commitCountsByPeriod).map((key) => new Date(key).getTime()))
+  )
+
+  const periodsWithZeroCommits: Record<string, number> = {}
+  const currentDate = new Date(earliestDate)
+  while (currentDate <= latestDate) {
+    const period =
+      mode === Mode.MONTH
+        ? `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}`
+        : currentDate.getFullYear().toString()
+    if (!(period in commitCountsByPeriod)) {
+      periodsWithZeroCommits[period] = 0
+    }
+    currentDate.setMonth(currentDate.getMonth() + (mode === Mode.MONTH ? 12 : 1))
+  }
+
+  const mergedCommitCounts = { ...periodsWithZeroCommits, ...commitCountsByPeriod }
 
   const sortedData = Object.entries(mergedCommitCounts)
-    .map(([monthYear, count]) => ({ date: new Date(monthYear), count }))
+    .map(([period, count]) => ({ date: new Date(period), count }))
     .sort((a, b) => a.date.getTime() - b.date.getTime())
 
   const xAxisData = sortedData.map(({ date }) => date)
   const seriesData = sortedData.map(({ count }) => count)
 
   return (
-    <Paper elevation={3} sx={{ height: '400px', margin: '20px 0px', padding: '15px' }}>
-      <Loading visible={loading} />
-      <NoData visible={!loading && noData} />
-      <CommitsChart seriesData={seriesData} visible={!loading && !noData} xAxisData={xAxisData} />
-    </Paper>
+    <Box>
+      <FormControl>
+        <RadioGroup onChange={(event) => setMode(event.target.value as Mode)} row value={mode}>
+          <FormControlLabel
+            control={<Radio />}
+            disabled={loading || noData}
+            label="Month"
+            value={Mode.MONTH}
+          />
+          <FormControlLabel
+            control={<Radio />}
+            disabled={loading || noData}
+            label="Year"
+            value={Mode.YEAR}
+          />
+        </RadioGroup>
+      </FormControl>
+      <Paper elevation={3} sx={{ height: '400px', margin: '20px 0px', padding: '15px' }}>
+        <Loading visible={loading} />
+        <NoData visible={!loading && noData} />
+        <CommitsChart
+          formatDate={formatDate}
+          label={getLabel()}
+          seriesData={seriesData}
+          visible={!loading && !noData}
+          xAxisData={xAxisData}
+        />
+      </Paper>
+    </Box>
   )
 }
 
--- src/components/Commits/CommitsChart.tsx
+++ src/components/Commits/CommitsChart.tsx
@@ -2,20 +2,24 @@ import { useTheme } from '@mui/material/styles'
 import { LineChart } from '@mui/x-charts'
 
 interface CommitChartProps {
+  formatDate: (date: Date) => string
+  label: string
   seriesData: number[]
   visible: boolean
   xAxisData: Date[]
 }
 
-const CommitChart = ({ seriesData, visible, xAxisData }: CommitChartProps): JSX.Element => {
+const CommitChart = ({
+  formatDate,
+  label,
+  seriesData,
+  visible,
+  xAxisData,
+}: CommitChartProps): JSX.Element => {
   const theme = useTheme()
 
   if (!visible) {
     return <></>
-  }
-
-  const formatDate = (date: Date) => {
-    return date.toLocaleDateString('en-US', { month: '2-digit', year: 'numeric' })
   }
 
   const uniqueYears = Array.from(new Set(xAxisData.map((date) => date.getFullYear())))
@@ -38,7 +42,7 @@ const CommitChart = ({ seriesData, visible, xAxisData }: CommitChartProps): JSX.
       xAxis={[
         {
           data: xAxisData,
-          label: ' \nTime',
+          label: label,
           scaleType: 'time',
           tickInterval: uniqueYears.map((year) => new Date(`${year}-01-01`)),
           valueFormatter: (date) => formatDate(date),
