9442b0bc1a20d06d595f92aa98013b32f93e1cbf
Riku Rauhala <riku@rauhala.io>
2024-06-02 10:18:56 +0300

    Install vite-plugin-instanbul

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,13 @@
+import coverageTask from '@cypress/code-coverage/task'
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
   retries: 3,
   e2e: {
     baseUrl: 'http://localhost:5173',
+    setupNodeEvents(on, config) {
+      coverageTask(on, config)
+      return config
+    },
   },
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import istanbul from 'vite-plugin-istanbul'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    istanbul({
+      cypress: true,
+      requireEnv: false,
+    }),
+  ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
